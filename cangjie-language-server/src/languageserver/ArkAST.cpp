3631b9877b85a983036b64a5cfe41b3089dff134
hua_yc <huayanchun@h-partners.com>
2025-10-16 15:39:11 +0800

    fix:update lsp
    Signed-off-by: hua_yc <huayanchun@h-partners.com>

--- cangjie-language-server/src/languageserver/ArkAST.cpp
+++ cangjie-language-server/src/languageserver/ArkAST.cpp
@@ -24,6 +24,10 @@ namespace ark {
 #define LIB_THIS THIS
 #undef THIS
 #endif
+
+
+
+    
 const std::unordered_set<TokenKind> CHECK_TOKEN_KIND = {TokenKind::IDENTIFIER, TokenKind::STRING_LITERAL,
                                                         TokenKind::MULTILINE_STRING, TokenKind::DOLLAR_IDENTIFIER,
                                                         TokenKind::INIT, TokenKind::SUPER, TokenKind::THIS,
